55b815ee18619fe297186fbe9a4392628648d854
freezeava <aashishava301@gmail.com>
2024-04-10 17:59:33 +0530

    object litrals concept

---
+++ javaScript/Objects/app.js
@@ -0,0 +1,21 @@
+
+
+let students= {
+
+  name:"Aman",
+  roll:34,
+  age:26,
+  sub:["phy","chem","maths"]
+
+};
+
+//random number generate function
+
+
+
+let num=Math.random();
+
+console.log(Math.floor(num*100)+1);// 1 to 100
+
+let num1=Math.random();
+console.log(Math.floor(num1*5)+1);
